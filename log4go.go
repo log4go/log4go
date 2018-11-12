4165d4d41456474b1c959601770bb6a21a6e3e46
Christian Pfarr <pfarrch@gmail.com>
2018-11-12 21:41:02 +0100

    Update log4go.go

--- log4go.go
+++ log4go.go
@@ -1,3 +1,4 @@
+/*
 Copyright 2018 log4go<at>freelists.org
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,5 +12,5 @@ Copyright 2018 log4go<at>freelists.org
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-
+*/
 package log4go
